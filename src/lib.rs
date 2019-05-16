fbd6a84f2f2ee232eb9029d55f357b2742d8c02b
Kenton Hamaluik <kenton@rehabtronics.com>
2019-05-16 14:39:16 -0600

    added wrapping versions

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@
 
 use std::collections::HashMap;
 
-/// Convenience function for hashing a hashable object using the std hashmap's default hasher
-pub fn base_hash<H>(obj: H) -> usize
+// convenience function for hashing a hashable object using the std hashmap's default hasher
+fn base_hash<H>(obj: H) -> usize
 where
     H: std::hash::Hash,
 {
@@ -14,6 +14,49 @@ where
     let mut hasher = DefaultHasher::new();
     obj.hash(&mut hasher);
     hasher.finish() as usize
+}
+
+fn calculate_q_floats(floats: &[f64], num_tilings: usize) -> Vec<isize> {
+    floats
+        .iter()
+        .map(|&x| (x * num_tilings as f64).floor() as isize)
+        .collect::<Vec<isize>>()
+}
+
+fn calculate_coords(tiling: usize, num_tilings: usize, q_floats: &Vec<isize>, ints: &Option<&[isize]>) -> Vec<isize> {
+    let tiling_x2 = tiling as isize * 2;
+    let mut coords = Vec::with_capacity(1 + q_floats.len());
+    coords.push(tiling as isize);
+    let mut b = tiling as isize;
+    for q in q_floats.iter() {
+        coords.push((q + b) / num_tilings as isize);
+        b += tiling_x2;
+    }
+    if let Some(ints) = ints {
+        coords.extend(*ints);
+    }
+
+    coords
+}
+
+fn calculate_coords_wrap(tiling: usize, num_tilings: usize, q_floats: &Vec<isize>, wrap_widths: &[Option<isize>], ints: &Option<&[isize]>) -> Vec<isize> {
+    let tiling_x2 = tiling as isize * 2;
+    let mut coords = Vec::with_capacity(1 + q_floats.len());
+    coords.push(tiling as isize);
+    let mut b = tiling as isize;
+    for (q, width) in q_floats.iter().zip(wrap_widths.iter()) {
+        let c: isize = (q + b % num_tilings as isize) / num_tilings as isize;
+        coords.push(match width {
+            Some(w) => c % w,
+            None => c,
+        });
+        b += tiling_x2;
+    }
+    if let Some(ints) = ints {
+        coords.extend(*ints);
+    }
+
+    coords
 }
 
 /// An index-hash-table, or IHT. It will allow to collect tile indices up to a
@@ -95,7 +138,7 @@ impl IHT {
     /// 
     /// The returned `Vec<usize>` is a vector containing exactly `num_tilings` elements, with each member being an index of a tile encoded by the function. Each member will always be >= 0 and <= size - 1.
     /// 
-    /// # Examples
+    /// # Example
     /// 
     /// ```
     /// # use tilecoding::IHT;
@@ -121,24 +164,11 @@ impl IHT {
     /// assert_eq!(indices, vec![9, 10, 11, 12, 13, 14, 15, 16]);
     /// ```
     pub fn tiles(&mut self, num_tilings: usize, floats: &[f64], ints: Option<&[isize]>) -> Vec<usize> {
-        let q_floats = floats
-            .iter()
-            .map(|&x| (x * num_tilings as f64).floor() as isize)
-            .collect::<Vec<isize>>();
+        let q_floats = calculate_q_floats(floats, num_tilings);
         let mut tiles: Vec<usize> = Vec::with_capacity(num_tilings + ints.unwrap_or(&[]).len());
 
         for tiling in 0..num_tilings {
-            let tiling_x2 = tiling as isize * 2;
-            let mut coords = Vec::with_capacity(1 + q_floats.len());
-            coords.push(tiling as isize);
-            let mut b = tiling as isize;
-            for q in q_floats.iter() {
-                coords.push((q + b) / num_tilings as isize);
-                b += tiling_x2;
-            }
-            if let Some(ints) = ints {
-                coords.extend(ints);
-            }
+            let coords = calculate_coords(tiling, num_tilings, &q_floats, &ints);
             tiles.push(self.get_index(coords));
         }
 
@@ -147,24 +177,87 @@ impl IHT {
 
     /// The same as the `tiles` function, except never insert or generate new indices. If an tiling calculate would result in a new tile, return `None` instead
     pub fn tiles_read_only(&mut self, num_tilings: usize, floats: &[f64], ints: Option<&[isize]>) -> Vec<Option<usize>> {
-        let q_floats = floats
-            .iter()
-            .map(|&x| (x * num_tilings as f64).floor() as isize)
-            .collect::<Vec<isize>>();
+        let q_floats = calculate_q_floats(floats, num_tilings);
         let mut tiles: Vec<Option<usize>> = Vec::with_capacity(num_tilings + ints.unwrap_or(&[]).len());
 
         for tiling in 0..num_tilings {
-            let tiling_x2 = tiling as isize * 2;
-            let mut coords = Vec::with_capacity(1 + q_floats.len());
-            coords.push(tiling as isize);
-            let mut b = tiling as isize;
-            for q in q_floats.iter() {
-                coords.push((q + b) / num_tilings as isize);
-                b += tiling_x2;
-            }
-            if let Some(ints) = ints {
-                coords.extend(ints);
-            }
+            let coords = calculate_coords(tiling, num_tilings, &q_floats, &ints);
+            tiles.push(self.get_index_read_only(coords));
+        }
+
+        tiles
+    }
+
+    /// A wrap-around version of `tiles`, described in the [original implementation](http://www.incompleteideas.net/tiles/tiles3.html#Wrap-around_Versions_).
+    /// 
+    /// # Arguments
+    /// 
+    /// * `num_tilings`—indicates the number of tile indices to be generated (i.e. the length of the returned `Vec`). This value hould be a power of two greater or equal to four times the number of floats according to the original implementation.
+    /// * `floats`—a list of floating-point numbers to be tiled
+    /// * `wrap_widths`—a list of optional integer wrapping points.
+    /// * `ints`—an optional list of integers that will also be tiled; all distinct integers will result in different tilings. In reinforcement learning, discrete actions are often provided here.
+    /// 
+    /// # Return Value
+    /// 
+    /// The returned `Vec<usize>` is a vector containing exactly `num_tilings` elements, with each member being an index of a tile encoded by the function. Each member will always be >= 0 and <= size - 1.
+    /// 
+    /// # Examples
+    /// 
+    /// From the [original implementation](http://www.incompleteideas.net/tiles/tiles3.html#Wrap-around_Versions_):
+    /// 
+    /// > The tilings we have discussed so far stretch out to infinity with no need to specify a range for them. This is cool, but sometimes not what you want. Sometimes you want the variables to wrap-around over some range. For example, you may have an angle variable that goes from 0 to 2π and then should wrap around, that is, you would like generalization to occur between angles just greater than 0 and angles that are nearly 2π. To accommodate this, we provide some alternative, wrap-around versions of the tiling routines.
+    /// >
+    /// > These versions take an additional input, wrap_widths, which parallels the float structure (array or list), and which specifies for each float the width of the range over which it wraps. If you don't want a float to wrap, it's wrap_width should be [`None`]. The wrap_width is in the same units as the floats, but should be an integer. This can be confusing, so let's do a simple 1D example. Suppose you have one real input, an angle theta. Theta is originally in radians, that is, in [0,2π), which you would like to wrap around at 2π. Remember that these functions all have their tile boundaries at the integers, so if you passed in theta directly there would be tile boundaries at 0, 1, and 2, i.e., just a few tiles over the whole range, which is probably not what you want. So let's say what we want! Suppose we want tilings with 10 tiles over the [0,2π) range. Then we have to scale theta so that it goes from 0 to 10 (instead of 0 to 2π). One would do this by multiplying theta by 10/2π. With the new scaled theta, the wrapping is over [0,10), for a wrap_width of 10.
+    /// 
+    /// Here is the code for the above case, assuming we want 16 tilings over the original [0, 2π) range with a memory size of 512:
+    /// 
+    /// ```
+    /// # use tilecoding::IHT;
+    /// # let theta: f64 = 0.0;
+    /// let mut iht = IHT::new(512);
+    /// iht.tiles_wrap(
+    ///     16,
+    ///     &[theta * 10. / (2.0 * std::f64::consts::PI)],
+    ///     &[Some(10)],
+    ///     None
+    /// );
+    /// ```
+    /// 
+    /// > Note that the code would be exactly the same if the original range of theta was [-π,+π]. Specifying the complete range of wrapping (rather than just the width) is not necessary for the same reason as we did not need to give a range at all in the previous routines.
+    /// >
+    /// > As another example, suppose you wanted to cover the 2π x 3 rectangular area with 16 tilings, each with a width of generalization one-tenth of the space in each dimension, and with wrap-around in the second dimension but not the first. In rust you would do:
+    /// 
+    /// ```
+    /// # use tilecoding::IHT;
+    /// # let x: f64 = 0.0;
+    /// # let y: f64 = 0.0;
+    /// let mut iht = IHT::new(512);
+    /// iht.tiles_wrap(
+    ///     16,
+    ///     &[x / (3. * 0.1), y / (2.0 * std::f64::consts::PI * 0.1)],
+    ///     &[None, Some(10)],
+    ///     None
+    /// );
+    /// ```
+    pub fn tiles_wrap(&mut self, num_tilings: usize, floats: &[f64], wrap_widths: &[Option<isize>], ints: Option<&[isize]>) -> Vec<usize> {
+        let q_floats = calculate_q_floats(floats, num_tilings);
+        let mut tiles: Vec<usize> = Vec::with_capacity(num_tilings + ints.unwrap_or(&[]).len());
+
+        for tiling in 0..num_tilings {
+            let coords = calculate_coords_wrap(tiling, num_tilings, &q_floats, wrap_widths, &ints);
+            tiles.push(self.get_index(coords));
+        }
+
+        tiles
+    }
+
+    /// The read-only version of `tiles_wrap`
+    pub fn tiles_wrap_read_only(&mut self, num_tilings: usize, floats: &[f64], wrap_widths: &[Option<isize>], ints: Option<&[isize]>) -> Vec<Option<usize>> {
+        let q_floats = calculate_q_floats(floats, num_tilings);
+        let mut tiles: Vec<Option<usize>> = Vec::with_capacity(num_tilings + ints.unwrap_or(&[]).len());
+
+        for tiling in 0..num_tilings {
+            let coords = calculate_coords_wrap(tiling, num_tilings, &q_floats, wrap_widths, &ints);
             tiles.push(self.get_index_read_only(coords));
         }
 
@@ -185,7 +278,7 @@ impl IHT {
 /// 
 /// The returned `Vec<usize>` is a vector containing exactly `num_tilings` elements, with each member being an index of a tile encoded by the function. Each member will always be >= 0 and <= size - 1.
 /// 
-/// # Examples
+/// # Example
 /// 
 /// ```
 /// # use tilecoding::tiles;
@@ -209,24 +302,75 @@ impl IHT {
 /// assert_eq!(indices, vec![638, 453, 557, 465, 306, 526, 281, 863]);
 /// ```
 pub fn tiles(size: usize, num_tilings: usize, floats: &[f64], ints: Option<&[isize]>) -> Vec<usize> {
-    let q_floats = floats
-        .iter()
-        .map(|&x| (x * num_tilings as f64).floor() as isize)
-        .collect::<Vec<isize>>();
+    let q_floats = calculate_q_floats(floats, num_tilings);
     let mut tiles: Vec<usize> = Vec::with_capacity(num_tilings + ints.unwrap_or(&[]).len());
 
     for tiling in 0..num_tilings {
-        let tiling_x2 = tiling as isize * 2;
-        let mut coords = Vec::with_capacity(1 + q_floats.len());
-        coords.push(tiling as isize);
-        let mut b = tiling as isize;
-        for q in q_floats.iter() {
-            coords.push((q + b) / num_tilings as isize);
-            b += tiling_x2;
-        }
-        if let Some(ints) = ints {
-            coords.extend(ints);
-        }
+        let coords = calculate_coords(tiling, num_tilings, &q_floats, &ints);
+        tiles.push(base_hash(coords) % size);
+    }
+
+    tiles
+}
+
+/// A wrap-around version of `tiles`, described in the [original implementation](http://www.incompleteideas.net/tiles/tiles3.html#Wrap-around_Versions_).
+/// 
+/// # Arguments
+/// 
+/// * `size`—the upper bounds of all returned indices
+/// * `num_tilings`—indicates the number of tile indices to be generated (i.e. the length of the returned `Vec`). This value hould be a power of two greater or equal to four times the number of floats according to the original implementation.
+/// * `floats`—a list of floating-point numbers to be tiled
+/// * `wrap_widths`—a list of optional integer wrapping points.
+/// * `ints`—an optional list of integers that will also be tiled; all distinct integers will result in different tilings. In reinforcement learning, discrete actions are often provided here.
+/// 
+/// # Return Value
+/// 
+/// The returned `Vec<usize>` is a vector containing exactly `num_tilings` elements, with each member being an index of a tile encoded by the function. Each member will always be >= 0 and <= size - 1.
+/// 
+/// # Examples
+/// 
+/// From the [original implementation](http://www.incompleteideas.net/tiles/tiles3.html#Wrap-around_Versions_):
+/// 
+/// > The tilings we have discussed so far stretch out to infinity with no need to specify a range for them. This is cool, but sometimes not what you want. Sometimes you want the variables to wrap-around over some range. For example, you may have an angle variable that goes from 0 to 2π and then should wrap around, that is, you would like generalization to occur between angles just greater than 0 and angles that are nearly 2π. To accommodate this, we provide some alternative, wrap-around versions of the tiling routines.
+/// >
+/// > These versions take an additional input, wrap_widths, which parallels the float structure (array or list), and which specifies for each float the width of the range over which it wraps. If you don't want a float to wrap, it's wrap_width should be [`None`]. The wrap_width is in the same units as the floats, but should be an integer. This can be confusing, so let's do a simple 1D example. Suppose you have one real input, an angle theta. Theta is originally in radians, that is, in [0,2π), which you would like to wrap around at 2π. Remember that these functions all have their tile boundaries at the integers, so if you passed in theta directly there would be tile boundaries at 0, 1, and 2, i.e., just a few tiles over the whole range, which is probably not what you want. So let's say what we want! Suppose we want tilings with 10 tiles over the [0,2π) range. Then we have to scale theta so that it goes from 0 to 10 (instead of 0 to 2π). One would do this by multiplying theta by 10/2π. With the new scaled theta, the wrapping is over [0,10), for a wrap_width of 10.
+/// 
+/// Here is the code for the above case, assuming we want 16 tilings over the original [0, 2π) range with a memory size of 512:
+/// 
+/// ```
+/// # use tilecoding::tiles_wrap;
+/// # let theta: f64 = 0.0;
+/// tiles_wrap(
+///     512,
+///     16,
+///     &[theta * 10. / (2.0 * std::f64::consts::PI)],
+///     &[Some(10)],
+///     None
+/// );
+/// ```
+/// 
+/// > Note that the code would be exactly the same if the original range of theta was [-π,+π]. Specifying the complete range of wrapping (rather than just the width) is not necessary for the same reason as we did not need to give a range at all in the previous routines.
+/// >
+/// > As another example, suppose you wanted to cover the 2π x 3 rectangular area with 16 tilings, each with a width of generalization one-tenth of the space in each dimension, and with wrap-around in the second dimension but not the first. In rust you would do:
+/// 
+/// ```
+/// # use tilecoding::tiles_wrap;
+/// # let x: f64 = 0.0;
+/// # let y: f64 = 0.0;
+/// tiles_wrap(
+///     512,
+///     16,
+///     &[x / (3. * 0.1), y / (2.0 * std::f64::consts::PI * 0.1)],
+///     &[None, Some(10)],
+///     None
+/// );
+/// ```
+pub fn tiles_wrap(size: usize, num_tilings: usize, floats: &[f64], wrap_widths: &[Option<isize>], ints: Option<&[isize]>) -> Vec<usize> {
+    let q_floats = calculate_q_floats(floats, num_tilings);
+    let mut tiles: Vec<usize> = Vec::with_capacity(num_tilings + ints.unwrap_or(&[]).len());
+
+    for tiling in 0..num_tilings {
+        let coords = calculate_coords_wrap(tiling, num_tilings, &q_floats, wrap_widths, &ints);
         tiles.push(base_hash(coords) % size);
     }
 
@@ -312,6 +456,15 @@ mod tests {
         iht.tiles(4, &[0.0], None);
         let indices = iht.tiles_read_only(4, &[32.0], None);
         assert_eq!(indices, vec![None, None, None, None]);
+    }
+
+    #[test]
+    fn wrapping_works() {
+        let mut iht = IHT::new(32);
+        let indices_1 = iht.tiles_wrap(4, &[0.0], &[Some(10)], None);
+        let indices_2 = iht.tiles_wrap(4, &[10.0], &[Some(10)], None);
+
+        assert_eq!(indices_1, indices_2);
     }
 
     /*#[bench]
